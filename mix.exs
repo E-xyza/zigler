ad29de974b8dd00bfb43b89772bc33738264c7de
ityonemo <isaac.yonemoto@gmail.com>
2019-12-09 22:40:41 -0800

    remove environment locks for dependencies

--- mix.exs
+++ mix.exs
@@ -30,9 +30,9 @@ defmodule Zigler.MixProject do
       # zigler's parsing is done using nimble_parsec
       {:nimble_parsec, "~> 0.5", runtime: false},
       # we need this for mix zigler.get_zig mix task.
-      {:mojito, "~> 0.5.0", only: :dev, runtime: false},
+      {:mojito, "~> 0.5.0", runtime: false},
       # documentation
-      {:ex_doc, "~> 0.21", only: [:dev, :doc], runtime: false},
+      {:ex_doc, "~> 0.21", runtime: false},
     ]
   end
 end
