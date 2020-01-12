156cc24c9e40a8440039a5e19deb58a47557f0e5
ityonemo <isaac.yonemoto@gmail.com>
2020-01-12 13:09:01 -0800

    stage 3: first step to beam-controlled asynchronous operation

--- test/async_test.exs
+++ test/async_test.exs
@@ -11,40 +11,50 @@ defmodule ZiglerTest.AsyncTest do
     right: i64,
     result: *i64,
   };
-  const ca = * const AsyncAdd;
 
   fn async_add_task(left: i64, right: i64) i64 {
     return left + right;
   }
 
-  fn async_add_wrapper(cache: ca) void {
+  const acp = * const AsyncAdd;
+
+  fn async_add_wrapper(cache: acp) void {
     cache.result.* = async_add_task(cache.left, cache.right);
   }
 
+  /// once again, do something terrible here.  We'll replace it with
+  /// something else in another stage.
+  var global_result: *i64 = undefined;
+
   /// nif: async_add/2
-  fn async_add(left: i64, right: i64) i64 {
-    var result = beam.allocator.create(i64) catch |_| return -1;
-    defer beam.allocator.destroy(result);
+  fn async_add(left: i64, right: i64) void {
+    global_result = beam.allocator.create(i64) catch |_| return;
 
     const cache = AsyncAdd{
       .left = left,
       .right = right,
-      .result = result
+      .result = global_result
     };
 
-    const thread = std.Thread.spawn(&cache, async_add_wrapper) catch |_| return -1;
-    thread.wait();
-    return cache.result.*;
+    const thread = std.Thread.spawn(&cache, async_add_wrapper) catch |_| return;
+    //thread.wait();
+    //return cache.result.*;
+  }
+
+  /// nif: async_fetch/0
+  fn async_fetch() i64 {
+    return global_result.*;
   }
   """
 
-  # STAGE 2: can we do a naive async operation with (thread-safe) memory mutation to
-  # return the value?
+  # STAGE 3: can we do a naive async operation with two hits (first hit is trigger)
+  # second hit is retrieve.
 
   @tag :one
   test "we can trigger the function" do
-    assert 47 == async_add(40, 7)
+    async_add(40, 7)
+    Process.sleep(100)
+    assert 47 = async_fetch()
   end
-
 
 end
