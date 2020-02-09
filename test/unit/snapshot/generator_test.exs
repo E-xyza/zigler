c599e0717836f34d575a8ab638383229c5ccb379
ityonemo <isaac.yonemoto@gmail.com>
2020-02-09 11:17:35 -0800

    initial snapshot working

--- test/unit/snapshot/generator_test.exs
+++ test/unit/snapshot/generator_test.exs
@@ -9,8 +9,75 @@ defmodule ZiglerTest.GeneratorTest do
 
   describe "the generator creates a reasonable shim" do
     test "for a single, zero arity function" do
+
+      code = """
+      // foo.exs line: 3
+
+      fn foo() i64 {
+        return 47;
+      }
+      """
+
       assert """
-      """ = Zig.generate(%Module{nifs: [@zeroarity]})
+      pub const e = @cImport({
+        @cInclude("erl_nif_zig.h");
+      });
+      const beam = @import("beam.zig");
+      const builtin = @import("builtin");
+      const std = @import("std");
+
+      // foo.exs line: 3
+
+      fn foo() i64 {
+        return 47;
+      }
+
+      extern fn __foo_shim__(env: beam.env, argc: c_int, argv: [*c] const beam.term) beam.term {
+        var res: beam.term = ___async_add(env, argc, argv) catch | err | {
+          if (err == beam.Error.FunctionClauseError) {
+            return beam.throw_function_clause_error(env);
+          } else if (err == error.OutOfMemory) {
+            return beam.throw_enomem(env);
+          } else {
+            return e.enif_make_badarg(env);
+          }
+        };
+        return res;
+      }
+
+      var exported_nifs = [1] e.ErlNifFunc{
+        e.ErlNifFunc{
+            .name = c"foo",
+            .arity = 0,
+            .fptr = __foo_shim__,
+            .flags = 0,
+        },
+      };
+
+      extern fn nif_load(env: beam.env, priv: [*c]?*c_void, load_info: beam.term) c_int {
+         return 0;
+      }
+
+      const entry = e.ErlNifEntry{
+          .major = 2,
+          .minor = 15,
+          .name = c"Elixir.Foo",
+          .num_of_funcs = 2,
+          .funcs = &(exported_nifs[0]),
+          .load = nif_load,
+          .reload = null,
+          .upgrade = null,
+          .unload = null,
+          .vm_variant = c"beam.vanilla",
+          .options = 1,
+          .sizeof_ErlNifResourceTypeInit = 24,
+          .min_erts = c"erts-10.4"
+      };
+
+      export fn nif_init() *const e.ErlNifEntry{
+          return &entry;
+      }
+      """ = Zig.generate(%Module{nifs: [@zeroarity], code: code, file: "foo.exs", module: Foo})
     end
   end
 end
