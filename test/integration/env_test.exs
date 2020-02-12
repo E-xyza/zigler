67182796fa9596ac26db49166b6c843c6f0ef281
ityonemo <isaac.yonemoto@gmail.com>
2020-02-12 01:53:03 -0800

    instantiated zero arity integration tests

--- test/integration/env_test.exs
+++ test/integration/env_test.exs
@@ -7,6 +7,26 @@ defmodule ZiglerTest.Integration.EnvTest do
   use Zigler
 
   ~Z"""
+  /// nif: zeroarity_with_env/0
+  fn zeroarity_with_env(env: beam.env) i64 {
+    return 47;
+  }
+
+  /// nif: zeroarity_with_erlnifenv/0
+  fn zeroarity_with_erlnifenv(env: ?*e.ErlNifEnv) i64 {
+    return 47;
+  }
+  """
+
+  test "for a zero arity function env variables are valid first parameters" do
+    assert 47 == zeroarity_with_env()
+  end
+
+  test "for a zero arity function erlnifenv variables are valid first parameters" do
+    assert 47 == zeroarity_with_erlnifenv()
+  end
+
+  ~Z"""
   /// nif: int_with_env/1
   fn int_with_env(env: beam.env, val: i64) i64 {
     return val;
