049912ea7c1d05d0e5df70f6205cf3423213999f
ityonemo <isaac.yonemoto@gmail.com>
2020-02-12 02:10:08 -0800

    and validated for e.erlnifterm

--- test/integration/beam_type_test.exs
+++ test/integration/beam_type_test.exs
@@ -2,7 +2,7 @@ defmodule ZiglerTest.Integration.BeamTypeTest do
   use ExUnit.Case, async: true
   use Zigler
 
-  #beam.term beam.atom beam.pid
+  #beam.atom beam.pid
 
   ~Z"""
   /// nif: pass_beam_term/1
@@ -16,6 +16,20 @@ defmodule ZiglerTest.Integration.BeamTypeTest do
     assert :foo == pass_beam_term(:foo)
     assert "bar" == pass_beam_term("bar")
     assert self() == pass_beam_term(self())
+  end
+
+  ~Z"""
+  /// nif: pass_e_erl_nif_term/1
+  fn pass_e_erl_nif_term(val: e.ErlNifTerm) e.ErlNifTerm {
+    return val;
+  }
+  """
+
+  test "generic e_erl_nif_term" do
+    assert 1 == pass_e_erl_nif_term(1)
+    assert :foo == pass_e_erl_nif_term(:foo)
+    assert "bar" == pass_e_erl_nif_term("bar")
+    assert self() == pass_e_erl_nif_term(self())
   end
 
 end
