91280fdf98082a0cfd48f1c60516de44d37b5b67
ityonemo <isaac.yonemoto@gmail.com>
2020-02-12 23:03:21 -0800

    verified also works with floats

--- test/integration/slice_type_egress_test.exs
+++ test/integration/slice_type_egress_test.exs
@@ -31,4 +31,33 @@ defmodule ZiglerTest.Integration.SliceTypeEgressTest do
     assert [47, 47, 47, 47] == egress_i64_slice()
   end
 
+  ~Z"""
+  var empty_f32 = [_]f32 { };
+  /// nif: egress_f32_slice/0
+  fn egress_f32_slice() []f32 {
+    var data = beam.allocator.alloc(f32, 4) catch return empty_f32[0..0];
+    // don't bother freeing it (this is normally a memory leak)
+
+    for (data) |*x| { x.* = 47.0; }
+
+    return data;
+  }
+
+  var empty_f64 = [_]f64 { };
+  /// nif: egress_f64_slice/0
+  fn egress_f64_slice() []f64 {
+    var data = beam.allocator.alloc(f64, 4) catch return empty_f64[0..0];
+    // don't bother freeing it (this is normally a memory leak)
+
+    for (data) |*x| { x.* = 47; }
+
+    return data;
+  }
+  """
+
+  test "egressing floats" do
+    assert [47.0, 47.0, 47.0, 47.0] == egress_f32_slice()
+    assert [47.0, 47.0, 47.0, 47.0] == egress_f64_slice()
+  end
+
 end
