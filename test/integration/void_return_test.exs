a53dc1c7d64470db25e2b7f98b3d62d0fd2b916a
ityonemo <isaac.yonemoto@gmail.com>
2020-02-17 22:00:36 -0800

    verify that void returns are ok for non-long nifs

---
+++ test/integration/void_return_test.exs
@@ -0,0 +1,16 @@
+defmodule ZiglerTest.Integration.VoidReturnTest do
+  use ExUnit.Case, async: true
+  use Zigler
+
+  ~Z"""
+  /// nif: void_return/1
+  fn void_return(env: beam.env, pid: beam.pid) void {
+    var _res = beam.send(env, pid, null, beam.make_atom(env, "done"));
+  }
+  """
+
+  test "a function can have a void return" do
+    void_return(self())
+    assert_receive :done
+  end
+end
