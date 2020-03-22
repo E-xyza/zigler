0803579720e85d50169aeb2eb58a2f073f5be2d6
ityonemo <isaac.yonemoto@gmail.com>
2020-03-21 17:48:42 -0700

    (failing) assert statements are now richer.

--- lib/zigler/parser/unit.ex
+++ lib/zigler/parser/unit.ex
@@ -3,12 +3,21 @@ defmodule Zigler.Parser.Unit do
   parses zig code and converts test blocks to test functions
   """
 
-  defstruct tests: []
+  defstruct tests: [], file: nil
 
   import NimbleParsec
 
   whitespace = ascii_string([?\s, ?\n], min: 1)
   blankspace = ascii_string([?\s], min: 1)
+
+  @close_paren 41
+
+  defcombinatorp :parenthetical,
+    string("(")
+    |> repeat(choice([
+      parsec(:parenthetical),
+      ascii_char(not: @close_paren)]))
+    |> string(")")
 
   init_context = empty()
   |> post_traverse(:init_context)
@@ -22,8 +31,18 @@ defmodule Zigler.Parser.Unit do
   |> ignore(string("\""))
   |> post_traverse(:parse_test_decl)
 
+  # assert statement
+  assert_parser = ignore(string("assert("))
+  |> repeat(choice([
+    parsec(:parenthetical),
+    ascii_char(not: @close_paren)]))
+  |> ignore(string(")"))
+  |> post_traverse(:parse_assert)
+
   if Mix.env == :test do
+    defparsec :test_parenthetical, parsec(:parenthetical)
     defparsec :test_decl_parser, concat(init_context, test_decl)
+    defparsec :test_assert_parser, concat(init_context, assert_parser)
   end
 
   test_line = optional(blankspace)
@@ -33,25 +52,38 @@ defmodule Zigler.Parser.Unit do
   |> optional(ascii_string([not: ?\n], min: 1))
   |> string("\n")
 
-  blank_line = optional(ascii_string([not: ?\n], min: 1))
+  normal_line = optional(
+    repeat(choice([
+      assert_parser,
+      ascii_char(not: ?\n)
+    ])))
   |> string("\n")
 
   unit_code = init_context
   |> repeat(choice([
     test_line,
-    blank_line]))
+    normal_line]))
 
   defparsec :unit_parser, unit_code
 
+  #############################################################################
+  ## POST-TRAVERSAL IMPLEMENTATIONS
+
   # ninjas in a context module for the context map.
-  defp init_context(_, _, _, _, _) do
-    {[], %__MODULE__{}}
+  defp init_context(_, _, c = %{file: _}, _, _) do
+    {[], struct(__MODULE__, c)}
   end
 
-  defp parse_test_decl(_rest, [test_name], context, _line, _offest) do
+  defp parse_test_decl(_rest, [test_name], context, _line, _offset) do
     test_nif = new_nif(test_name)
     {["fn #{test_nif.name}() !void"],
       %{context | tests: [test_nif | context.tests]}}
+  end
+
+  defp parse_assert(_rest, content, context, {line, _char}, _offset) do
+    test_content = content |> Enum.reverse |> IO.iodata_to_binary
+    rewritten = "assert(#{test_content}, \"#{context.file}\", #{line})"
+    {[rewritten], context}
   end
 
   alias Zigler.Parser.Nif
--- test/unit/parser/unit_test.exs
+++ test/unit/parser/unit_test.exs
@@ -12,7 +12,7 @@ defmodule ZiglerTest.Parser.UnitTest do
   }
 
   test "#{@test_name}" {
-    assert(47 == 47);
+    assert(47 == forty_seven());
   }
   """
 
@@ -23,12 +23,39 @@ defmodule ZiglerTest.Parser.UnitTest do
     hash = Zigler.Unit.name_to_hash("foobar")
     code = "fn test_#{hash}() !void"
     assert {:ok, [^code], "", %{tests: [%Nif{test: "foobar"}]}, _, _} =
-      Unit.test_decl_parser("test \"foobar\"")
+      Unit.test_decl_parser("test \"foobar\"", context: %{file: "foo.zig"})
   end
 
+  describe "parenthetical descriptions work" do
+    test "in the basic case" do
+      assert {:ok, data, _, _, _, _} = Unit.test_parenthetical("(foo)")
+      assert "(foo)" = IO.iodata_to_binary(data)
+    end
+    test "in the complex case" do
+      assert {:ok, data, _, _, _, _} = Unit.test_parenthetical("(fo(bar)o)")
+      assert "(fo(bar)o)" = IO.iodata_to_binary(data)
+    end
+    test "with leftovers" do
+      assert {:ok, data, rest, _, _, _} = Unit.test_parenthetical("(fo(bar)o) baz")
+      assert "(fo(bar)o)" = IO.iodata_to_binary(data)
+      assert " baz" = rest
+    end
+  end
+
+  describe "asserts are properly detected and rewritten" do
+    test "in the basic case" do
+      assert {:ok, [rewritten], _, _, _, _} = Unit.test_assert_parser("assert(1 == 1)", context: %{file: "foo.zig"})
+      assert rewritten == "assert(1 == 1, \"foo.zig\", 1)"
+    end
+    test "with nested parentheticals" do
+      assert {:ok, [rewritten], _, _, _, _} = Unit.test_assert_parser("assert(1 == bar())", context: %{file: "foo.zig"})
+      assert rewritten == "assert(1 == bar(), \"foo.zig\", 1)"
+    end
+  end
 
   test "a unit test is correctly converted" do
-    {:ok, code, "", context, _, _} = Unit.unit_parser(@core_doc)
+    {:ok, code, "", context, _, _} =
+      Unit.unit_parser(@core_doc, context: %{file: "foo.zig"})
 
     assert [%Nif{test: @test_name}] = context.tests
 
@@ -41,10 +68,8 @@ defmodule ZiglerTest.Parser.UnitTest do
     }
 
     fn test_#{Zigler.Unit.name_to_hash @test_name}() !void {
-      assert(47 == 47);
+      assert(47 == forty_seven(), "foo.zig", 9);
     }
     """
   end
-
-
 end
