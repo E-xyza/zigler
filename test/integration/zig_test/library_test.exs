bc8b1a848acd913e58a31eec3cbff3d2d3e9f5f4
ityonemo <isaac.yonemoto@gmail.com>
2020-03-22 15:43:27 -0700

    rename import test to library test (because we're going to actually do an import test next)

--- test/integration/zig_test/library_test.exs
+++ test/integration/zig_test/library_test.exs
@@ -1,7 +1,7 @@
 #guard against this cblas.h not existing.
 if File.exists?("/usr/include/x86_64-linux-gnu/cblas.h") do
 
-defmodule ZiglerTest.Integration.ZigTest.ImportTest do
+defmodule ZiglerTest.Integration.ZigTest.LibraryTest do
   use ExUnit.Case
   use Zigler
 
