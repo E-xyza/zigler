b174d5680068491d2ab300fc9f85d44b4b0174ab
ityonemo <isaac.yonemoto@gmail.com>
2020-03-22 15:54:54 -0700

    set up the importation scheme

---
+++ test/integration/zig_test/import_test.exs
@@ -0,0 +1,22 @@
+defmodule ZiglerTest.Integration.ZigTest.ImportTest do
+  use ExUnit.Case
+
+  use Zigler
+
+  import Zigler.Unit
+
+  @moduletag :zigtest
+
+  # imports module support/passing_tests.exs into zigler.
+  # note that this should be precompiled as a result of being in
+  # test/support directory.
+  zigtest ZiglerTest.ZigTest.ImportTest
+
+  # make sure the existing module recapitulates the code from the
+  # tested module
+
+  test "this module has the the transitive test" do
+    assert function_exported?(__MODULE__, :"transitive zigtest", 0)
+  end
+
+end
