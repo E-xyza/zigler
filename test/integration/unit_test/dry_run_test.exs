d304e80bedf96025c26d059b49f759c8ebe8279c
ityonemo <isaac.yonemoto@gmail.com>
2020-05-25 16:45:59 -0700

    move unit test dry run to integration (as it builds)

---
+++ test/integration/unit_test/dry_run_test.exs
@@ -0,0 +1,36 @@
+defmodule ZiglerTest.ZigT do
+  # a module with a single zig test
+
+  use Zigler, dry_run: true
+
+  ~Z"""
+  /// nif: foo/0
+  fn foo() i32 { return 47; }
+
+  test "foo returns 47" {
+    assert(47 == foo());
+  }
+  """
+end
+
+defmodule ZiglerTest.Integration.UnitTest.DryRunTest do
+  # checks which make sure our code looks good.
+  use ExUnit.Case, async: true
+
+  # we don't suffix this with "Test" so it will be a normal
+  # elixir module, and its failure (due to not being loaded) is
+  # immaterial.
+
+  defmodule NotExecuted do
+    #use ExUnit.Case, async: true
+    use Zigler, dry_run: true
+    import Zigler.Unit
+
+    zigtest ZiglerTest.ZigT, dry_run: true
+  end
+
+  test "dry run is successful" do
+    assert {:"foo returns 47", 0} in NotExecuted.__info__(:functions)
+  end
+
+end
