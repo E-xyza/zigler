faa0e6ebe8a7c2e56c0a9f3b00d1f0999c705403
ityonemo <isaac.yonemoto@gmail.com>
2020-05-25 14:11:52 -0700

    verup with new pre designation

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Zigler.MixProject do
 
     [
       app: :zigler,
-      version: "0.3.0-pre",
+      version: "0.3.0-pre2",
       elixir: "~> 1.9",
       start_permanent: env == :prod,
       elixirc_paths: elixirc_paths(env),
