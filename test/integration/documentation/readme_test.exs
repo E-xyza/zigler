bdfae3bb870839dcec03d9ca99636d47bc7e99ef
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-02-17 18:35:12 -0800

    change default to nil

--- test/integration/documentation/readme_test.exs
+++ test/integration/documentation/readme_test.exs
@@ -3,7 +3,7 @@ if File.exists?("/usr/include/x86_64-linux-gnu/cblas.h") and
    File.exists?("/usr/lib/x86_64-linux-gnu/blas/libblas.so") do
 
 # prevent CI from running this
-unless System.get_env("RUNNING_CI", false) do
+unless System.get_env("RUNNING_CI", nil) do
 
 defmodule ZiglerTest.Integration.Documentation.ReadmeTest do
   use ExUnit.Case, async: true
