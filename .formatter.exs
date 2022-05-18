1b97a6df1d6e3a0a19c7847a7c6dabbe8a539847
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2022-05-17 20:33:56 -0500

    add formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,12 @@
+default_locals = [
+  defdelegate: 1,
+  throw: 1
+]
+
+[
+  inputs: ~w(
+    {mix,.formatter,.credo}.exs
+    {config,lib,rel,test}/**/*.{ex,exs}
+  ),
+  locals_without_parens: default_locals
+]
