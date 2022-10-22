c07a5464ff6ab948780500b833b41c255681ffeb
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2022-10-22 17:22:24 -0500

    version up

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Zigler.MixProject do
 
     [
       app: :zigler,
-      version: "0.9.1",
+      version: "0.9.2",
       elixir: "~> 1.9",
       start_permanent: env == :prod,
       elixirc_paths: elixirc_paths(env),
