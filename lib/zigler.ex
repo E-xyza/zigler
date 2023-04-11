3bd7387e18ceaae67c33c9a5fbcc1c2451d05930
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-04-11 13:33:18 -0500

    generalizes building skeletons for ast

--- lib/zigler.ex
+++ lib/zigler.ex
@@ -1,16 +1,18 @@
 defmodule :zigler do
   def parse_transform(ast, _opts) do
-    {code, pos} = case Enum.find(ast, &match?({:attribute, _, :zig, _}, &1)) do
-      nil -> raise "No zig code found"
-      {:attribute, pos, :zig, code} -> {code, pos}
-    end
+    {code, pos} =
+      case Enum.find(ast, &match?({:attribute, _, :zig, _}, &1)) do
+        nil -> raise "No zig code found"
+        {:attribute, pos, :zig, code} -> {code, pos}
+      end
 
-    opts = case Enum.find(ast, &match?({:attribute, _, :zig_opts, _}, &1)) do
-      nil -> []
-      {:attribute, _, :zig_opts, opts} -> opts
-    end
+    opts =
+      case Enum.find(ast, &match?({:attribute, _, :zig_opts, _}, &1)) do
+        nil -> raise "No zig opts found"
+        {:attribute, _, :zig_opts, opts} -> opts
+      end
 
-    {code, pos, opts} |> IO.inspect
+    {code, pos, opts} |> IO.inspect()
 
     ast
   end
