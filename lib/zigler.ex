c27077e4c0af6996f51cd9f8df5bb01fb939f365
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-04-11 12:57:49 -0500

    extracts zig compiler instructions out of the parse transform info

--- lib/zigler.ex
+++ lib/zigler.ex
@@ -1,7 +1,17 @@
 defmodule :zigler do
-  def parse_transform(ast, opts) do
-    ast |> IO.inspect
-    opts |> IO.inspect
+  def parse_transform(ast, _opts) do
+    {code, pos} = case Enum.find(ast, &match?({:attribute, _, :zig, _}, &1)) do
+      nil -> raise "No zig code found"
+      {:attribute, pos, :zig, code} -> {code, pos}
+    end
+
+    opts = case Enum.find(ast, &match?({:attribute, _, :zig_opts, _}, &1)) do
+      nil -> []
+      {:attribute, _, :zig_opts, opts} -> opts
+    end
+
+    {code, pos, opts} |> IO.inspect
+
     ast
   end
 end
