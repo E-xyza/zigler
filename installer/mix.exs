75cc61efd4880efdfcf64f63d9bdb38c6d6d456e
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-12-20 10:48:18 -0600

    fix to enable publishing of mix installer

--- installer/mix.exs
+++ installer/mix.exs
@@ -42,7 +42,7 @@ defmodule Zig.Get.MixProject do
 
   def deps do
     [
-      {:ex_doc, "~> 0.24", only: :docs},
+      {:ex_doc, "~> 0.24", only: :dev},
       {:minisign, "~> 0.1"},
       {:floki, "~> 0.35"}
     ]
