95b42c7d263c556f96121b00bc0b7341a2caf607
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-03-31 12:16:29 -0500

    windows equivalent error?

--- test/compiler_error/syntax_error_test.exs
+++ test/compiler_error/syntax_error_test.exs
@@ -2,9 +2,21 @@ defmodule ZiglerTest.CompilerError.SyntaxErrorTest do
   use ExUnit.Case, async: true
 
   describe "sema causes an error" do
+
+    case :os.type() do
+      {_, :nt} ->
+        @syntax_error "test\\compiler_error\\.Elixir.ZiglerTest.CompilerError.SyntaxError.zig:5:12: error: expected ';' after statement\r\n\r\n\r\n  return 42 // note a semicolon is missing here.\r\n\r\n\r\n           ^"
+      _ -> 
+        @syntax_error """
+        test/compiler_error/_syntax_error.exs:8: error: expected ';' after statement
+          return 42 // note a semicolon is missing here.
+                     ^
+        """
+    end
+
     test "when the zig syntax is wrong" do
       assert_raise CompileError,
-                   "test/compiler_error/_syntax_error.exs:8: error: expected ';' after statement\n\n\n\n  return 42 // note a semicolon is missing here.\n\n\n           ^",
+                   @syntax_error,
                    fn ->
                      Code.compile_file("_syntax_error.exs", __DIR__)
                    end
