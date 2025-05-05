d88e910f8bf5d70800ba4f3e0737e7bd054dc43d
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-05-05 12:14:51 -0400

    removed c options normalization from options module

--- lib/zig/_c.ex
+++ lib/zig/_c.ex
@@ -34,14 +34,126 @@ defmodule Zig.C do
 
   def new(opts, context) do
     opts
-    |> Options.normalize_pathlist(:include_dirs, context)
-    |> Options.normalize_pathlist(:library_dirs, context)
-    |> Options.normalize_pathlist(:link_lib, context)
+    |> Options.normalize(:include_dirs, [], &normalize_pathlist/2, context)
+    |> Options.normalize(:library_dirs, [], &normalize_pathlist/2, context)
+    |> Options.normalize(:link_lib, [], &normalize_pathlist/2, context)
+    |> Options.normalize(:src, [], &normalize_c_src/2, context)
     |> Options.validate(:link_libcpp, :boolean, context)
-    |> Options.normalize_c_src(:src, context)
     |> then(&struct!(__MODULE__, &1))
   rescue
     e in KeyError ->
       Options.raise_with("was supplied the invalid option `#{e.key}`", context)
+  end
+
+  def normalize_pathlist([n | _] = charlist, context) when is_integer(n),
+    do: [resolve_path("#{charlist}", context)]
+
+  def normalize_pathlist(path_or_paths, context) do
+    path_or_paths
+    |> List.wrap()
+    |> Enum.map(&resolve_path(&1, context))
+  end
+
+  defp resolve_path({:system, path}, context) do
+    {:system, IO.iodata_to_binary(path)}
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with("system path must be iodata", path, context)
+  end
+
+  defp resolve_path({:priv, path}, context) do
+    path
+    |> IO.iodata_to_binary()
+    |> Path.expand(:code.priv_dir(context.otp_app))
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with("priv path must be iodata", path, context)
+  end
+
+  defp resolve_path(path, context) do
+    path
+    |> IO.iodata_to_binary()
+    |> Path.expand(Path.dirname(context.file))
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with(
+        "must be path, `{:priv, path}`, `{:system, path}`, or a list of those",
+        path,
+        context
+      )
+  end
+
+  def normalize_c_src([n | _] = charlist, context) when is_integer(n),
+    do: [{resolve_path("#{charlist}", context), []}]
+
+  def normalize_c_src(src_or_srcs, context) do
+    src_or_srcs
+    |> List.wrap()
+    |> Enum.flat_map(fn src_def ->
+      src_def
+      |> resolve_src(context)
+      |> expand_wildcards()
+    end)
+  end
+
+  defp resolve_src({tag, _} = path, context) when tag in ~w[priv system]a do
+    path
+    |> resolve_path(context)
+    |> normalize_src_options([], context)
+  end
+
+  defp resolve_src({tag, path, options}, context) when tag in ~w[priv system]a do
+    {tag, path}
+    |> resolve_path(context)
+    |> normalize_src_options(options, context)
+  end
+
+  defp resolve_src({path, options}, context) do
+    path
+    |> IO.iodata_to_binary()
+    |> Path.expand(Path.dirname(context.file))
+    |> normalize_src_options(options, context)
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with("source files must be iodata", path, context)
+  end
+
+  defp resolve_src(path, context) do
+    path
+    |> IO.iodata_to_binary()
+    |> Path.expand(Path.dirname(context.file))
+    |> normalize_src_options([], context)
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with(
+        "must be path, `{path, [opts]}`, `{:priv, path}`, `{:priv, path, [opts]}`, `{:system, path}`, `{:system, path, [opts]}`, or a list of those",
+        path,
+        context
+      )
+  end
+
+  defp normalize_src_options(path, opts, context) when is_list(opts) do
+    {path, Enum.map(opts, &IO.iodata_to_binary/1)}
+  rescue
+    _ in ArgumentError ->
+      Options.raise_with("must be a list of iodata", opts, Options.push_key(context, inspect(path)))
+  end
+
+  defp expand_wildcards({{:system, path}, opts}) do
+    {path, opts}
+    |> expand_wildcards()
+    |> Enum.map(fn {path, opts} -> {{:system, path}, opts} end)
+  end
+
+  defp expand_wildcards({path, opts} = spec) do
+    if String.ends_with?(path, "/*") do
+      dir = Path.dirname(path)
+
+      dir
+      |> File.ls!()
+      |> Enum.map(&{Path.join(dir, &1), opts})
+    else
+      [spec]
+    end
   end
 end
