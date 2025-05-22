74427a8ee7486c5f5ebb57540b0f4549e4778891
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-05-22 00:14:20 -0400

    adds test against c_char allocation (#541)

---
+++ test/allocator/c_char_test.exs
@@ -0,0 +1,20 @@
+defmodule ZiglerTest.Allocator.CustomFunctionTest do
+  use ExUnit.Case, async: true
+
+  use Zig, otp_app: :zigler
+
+  ~Z"""
+  const beam = @import("beam");
+  pub fn foo() ![]c_char {
+      const slice = try beam.allocator.alloc(c_char, 16);
+      for (slice) |*c| {
+          c.* = 'a';
+      }
+      return slice;
+  }
+  """
+
+  test "allocating c_char" do 
+    assert ~C[aaaaaaaaaaaaaaaa] = foo()
+  end
+end
