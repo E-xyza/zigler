dfd82525a2c7c1ae6bdb3e64a37f31fcbeccaa9e
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-09-04 00:13:10 -0500

    actual test conditions on precompiled_test

--- test/precompiled_test.exs
+++ test/precompiled_test.exs
@@ -1,3 +1,5 @@
+File.rm_rf!(Zig.Builder.staging_directory(ZiglerTest.PrecompiledTest))
+
 defmodule ZiglerTest.Compiled do
   use Zig, otp_app: :zigler
 
@@ -13,5 +15,19 @@ defmodule ZiglerTest.PrecompiledTest do
 
   require ZiglerTest.Compiled
 
-  use Zig, otp_app: :zigler, precompiled: "./priv/"
+  use Zig, otp_app: :zigler, precompiled: "./priv/lib/Eixir.ZiglerTest.Compiled.so"
+
+  ~Z"""
+  pub fn add_one(x: u32) u32 {
+    return x + 1;
+  }
+  """
+
+  test "staging directory doesn't exist" do
+    refute File.dir?(Zig.Builder.staging_directory(ZiglerTest.PrecompiledTest))
+  end
+
+  test "function works" do
+    assert add_one(47) == 48
+  end
 end
