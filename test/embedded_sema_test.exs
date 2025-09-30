fa8dbbda467b97b74bbbcccea0573729b244b548
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-09-30 18:23:19 -0500

    resolves memory-mapping segfault in linux

--- test/embedded_sema_test.exs
+++ test/embedded_sema_test.exs
@@ -1,9 +1,4 @@
-defmodule ZiglerTest.EmbeddedSemaTest do
-  require Logger
-
-  alias Zig.Sema
-
-  use ExUnit.Case, async: true
+defmodule ZiglerTest.EmbeddedSema do
   use Zig, otp_app: :zigler
 
   ~Z"""
@@ -11,13 +6,25 @@ defmodule ZiglerTest.EmbeddedSemaTest do
       return x + 1;
   }
   """
+end
+
+defmodule ZiglerTest.EmbeddedSemaTest do
+  require Logger
+
+  alias Zig.Sema
+
+  use ExUnit.Case, async: true
 
   test "linksection" do
+    # this must be purged otherwise if a memory-mapped, dlloaded so file is read
+    # by different mechanism a linux gets really upset and causes a segfault.
+    :code.purge(ZiglerTest.EmbeddedSema)
+
     file =
       :zigler
       |> :code.priv_dir()
       |> Path.join("lib")
-      |> Path.join("#{__MODULE__}.so")
+      |> Path.join("ZiglerTest.EmbeddedSema.so")
 
     {_, sema_text} = Sema._obtain_precompiled_sema_json(%{precompiled: file})
     assert %{"functions" => [%{"name" => "add_one"}]} = JSON.decode!(sema_text)
