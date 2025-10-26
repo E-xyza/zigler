d949c037d89f12b55897057ea20d99097e0119d4
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-10-26 14:28:46 -0500

    disable error return traces in windows (for threaded)

--- priv/beam/threads.zig
+++ priv/beam/threads.zig
@@ -206,7 +206,10 @@ pub fn Thread(comptime function: anytype) type {
                         if (@intFromError(err) == TERMINATED) {
                             result_ptr.* = .{ .error_return_trace = beam.make_empty_list(.{}) };
                         } else {
-                            const response = .{ .@"error", err, @errorReturnTrace() };
+                            const response = if (@import("builtin").os.tag == .windows)
+                                .{ .@"error", err, null }
+                            else
+                                .{ .@"error", err, @errorReturnTrace() };
                             result_ptr.* = .{ .error_return_trace = beam.make(response, .{}) };
                         }
                     }
